598aaa8cfa2162e3c668023f9689ceaa8e4421d9
binJamin <jacobvraffe@gmail.com>
2019-10-25 15:07:02 -0500

    Adding the JavaScript file that does all the work

---
+++ scrip.js
@@ -0,0 +1,45 @@
+const softwareData = [
+    {
+      title: "Adobe Acrobat DC",
+      description: "Adobe Acrobat is a family of application software and Web services developed by Adobe Inc.\
+      to view, create, manipulate, print and manage files in Portable Document Format. The family comprises Acrobat Reader, Acrobat and Acrobat.com.",
+      icon: "Assets/adobe.png"
+    },
+    {
+        title: "Avaya",
+        description: "Avaya Equinox for Windows turns your Windows PC into a powerful communications and collaboration\
+        system that can work in conjunction with your Avaya deskphone or enable you\
+        to work without compromise from anywhere. It provides SIP-based Voice-over-IP, IM/presence, web \
+        conferencing and point-to-point and multiparty video and easy to use \
+        contact centric workflows with contextual controls.",
+        icon: "Assets/avaya.png"
+      },
+      {
+        title: "Bananatag",
+        description: "Bananatag is an email tracking software launched in 2011. \
+        It notifies users whether the emails sent are successfully delivered and opened. Bananatag is used in email marketing where mass emails are sent to millions of targeted customers are response awaited.",
+        icon: "Assets/bananatag.png"
+      },
+      
+  ];
+
+ function softTemplate(item) {
+    return `
+      <card class="nested">
+        <img class="image" src="${item.icon}">
+          <div class="text">
+          <h3>${item.title}</h3>
+          <p>slice.(100,${item.description})</p>
+        </div>
+      </card>
+    `;
+ }
+
+  document.getElementById("app").innerHTML = `
+  <div class="stack">
+  <h1 class="app-title">Software Request</h1>
+    <main class="grid">
+      ${softwareData.map(softTemplate).join("")}
+    </main>
+  </div>
+  `;
