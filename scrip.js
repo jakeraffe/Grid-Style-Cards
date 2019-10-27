a386169050d540b0949a963b2c1053b349b39bee
Jake Raffe <43254777+jakeraffe@users.noreply.github.com>
2019-10-26 22:40:36 -0500

    More Javascript
    Started working on the "Read More" function. In order to do this I needed to first figure out how to split up the text and format it correctly, than secondly, how to pass the template literal description into a function.

--- scrip.js
+++ scrip.js
@@ -1,65 +1,82 @@
 const softwareData = [
     {
       title: "Adobe Acrobat DC",
-      description: "Avaya Equinox for Windows turns your Windows PC into a powerful communications and collaboration\
-      system that can work in conjunctio",
+      description: "Microsoft Visual Studio is an integrated development environment from Microsoft. \
+It is used to develop computer programs, as well as websites, web apps, web services and mobile apps",
       icon: "Assets/adobe.png"
     },
     {
         title: "Visual Studio",
-        description: "Avaya Equinox for Windows turns your Windows PC into a powerful communications and collaboration\
-        system that can work in conjunctio",
+        description: "Avaya Equinox for Windows turns your Windows PC into a powerful communications and collaboration \
+system that can work in conjunction",
         icon: "Assets/vs.png"
       },
       {
         title: "Visual Studio Code",
-        description: "Microsoft Visual Studio is an integrated development environment from Microsoft. \
-        It is used to develop computer programs, as well as websites, web apps, web services and mobile apps",
+        description: "Microsoft Visual Studio is an integrated development environment from Microsoft. It is used to develop computer programs, \
+as well as websites, web apps, web services and mobile apps",
         icon: "Assets/vscode.png"
       },
       {
         title: "Bananatag",
-        description: "Avaya Equinox for Windows turns your Windows PC into a powerful communications and collaboration system that can work in conjunctio",
+        description: "Avaya Equinox for Windows turns your Windows PC into a powerful communications and collaboration \
+system that can work in conjunctio",
         icon: "Assets/avaya.png"
       },
       {
         title: "Visual Studio Code",
-        description: "Microsoft Visual Studio is an integrated development environment from Microsoft. It is used to develop computer programs, as well as websites, web apps, web services and mobile apps",
+        description: "Microsoft Visual Studio is an integrated development environment from Microsoft. \
+It is used to develop computer programs, as well as websites, web apps, web services and mobile apps",
         icon: "Assets/vscode.png"
       },
       {
         title: "Visual Studio Code",
-        description: "Microsoft Visual Studio is an integrated development environment from Microsoft. It is used to develop computer programs, as well as websites, web apps, web services and mobile apps",
+        description: "Microsoft Visual Studio is an integrated development environment from Microsoft. It is used to develop computer programs, \
+as well as websites, web apps, web services and mobile apps",
         icon: "Assets/vscode.png"
       },
       {
         title: "Visual Studio Code",
-        description: "Microsoft Visual Studio is an integrated development environment from Microsoft. It is used to develop computer programs, as well as websites, web apps, web services and mobile apps",
+        description: "Microsoft Visual Studio is an integrated development environment from Microsoft. \
+It is used to develop computer programs, as well as websites, web apps, web services and mobile apps",
         icon: "Assets/vscode.png"
       }
   ];
 
- function getDescription(desc){
-  var newDesc = desc.split(" ");
-  newDesc.join(' ');
-  alert(newDesc);
- }
- function softTemplate(item) {
+  
+
+  function getDesc(itemDesc){
+    let desc = itemDesc.description;
+
+    let newDesc = desc.split(" ");    //sends string to array than removes commas and splits by number of words
+    newDesc = newDesc.join(" ");
+
+      let firstHalf = newDesc.slice(0,100);   //gets the first 100 characters 
+      firstHalf = firstHalf + " . . .";       //append three dots after
+      // alert(firstHalf);   
+
+      let secondHalf = newDesc.slice(101);    //gets the remainder of the sliced string
+      // alert(secondHalf);
+  }
+
+  function softTemplate(item) {
     return `
       <card class="nested">
         <img class="image" src="${item.icon}">
           <div class="text">
-          <h3>${item.title}</h3>
-          <p>${item.description}</p>
-          <button class="request-button" onclick="getDescription('${item.description}')">Request</button>
+            <!--<button class="request-button" onclick="getDescription('${item.description}')">Request</button> -->
+            <h3>${item.title}</h3>
+            <p>${item.description}</p>
+            <button class="readmore" onclick="getDesc()">Read More</button>
         </div>
       </card>
     `;
- }
+  }
 
   document.getElementById("app").innerHTML = `
   <div class="stack">
     <main class="grid">
+      ${softwareData.map(getDesc).join("")}
       ${softwareData.map(softTemplate).join("")}
     </main>
   </div>
