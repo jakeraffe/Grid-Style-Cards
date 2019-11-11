1ab1d7d7dd969ba788a5c99035eb491b8430a8df
binJamin <jraffe2@uic.edu>
2019-11-11 14:34:16 -0600

    Figured out how to categorize the lists based off the menu choice
    The code is a little messy though. Will need to work on cleaning it up and shorten it. There is a lot of copied and pasted code here so well see if we can make it less messy.

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,9 @@
 
 const main_Category_View = [
   {
-    title: "Wooodward",
+    title: "Woodward",
     description: "Apps build by Woodward specifically for Woodward",
-    icon: "Assets/docker1.png"
+    icon: "Assets/woodward.jpg"
   },
   {
     title: "Office",
@@ -13,20 +13,19 @@ const main_Category_View = [
   {
     title: "Engineering",
     description: "From Autocad to something else that ill write in later, all your Enginerring Software is found here",
-    icon: "Assets/docker1.png"
+    icon: "Assets/engineering.jpg"
   }
 ];
 
 
 const approved_Office_Software = [
   {
-    title: "Docker",
-    description: "Docker is a set of platform-as-a-service products that use OS-level virtualization to deliver software\
-in packages or containers. Containers are isolated from one another and bundle their own software.",
-    icon: "Assets/docker1.png"
+    title: "Office Microsoft Word",
+    description: "Microsoft Word is a word processor developed by Microsoft. It was first released on October 25, 1983 under the name Multi-Tool Word for Xenix systems.",
+    icon: "Assets/word.png"
   },
   {
-    title: "Kubernetes",
+    title: "Office Kubernetes",
     description: "Kubernetes is an open-source container-orchestration system for automating application deployment, scaling, \
 and management. It is now maintained by the Cloud Native Computing Foundation.",
     icon: "Assets/kuber2.png"
@@ -36,19 +35,19 @@ and management. It is now maintained by the Cloud Native Computing Foundation.",
 
 const approved_Engineering_Software = [
 {
-  title: "Microsoft Azure",
+  title: "Engineering Microsoft Azure",
   description: "Microsoft Azure is a cloud computing service created by Microsoft for building, testing, deploying, and managing\
 applications and services through Microsoft-managed data centers.",
   icon: "Assets/azure.png"
 },
   {
-    title: "Visual Studio Code",
+    title: " Engineering Visual Studio Code",
     description: "Visual Studio Code is a source-code editor developed by Microsoft for Windows, Linux and macOS. It includes support \
 for debugging, embedded Git control and GitHub, and code refactoring.",
     icon: "Assets/vscode.png"
   },
   {
-    title: "Visual Studio",
+    title: "Engineering Visual Studio",
     description: "Microsoft Visual Studio is an integrated development environment from Microsoft. \
 It is used to develop computer programs, as well as websites, web apps, web services and mobile apps.",
     icon: "Assets/vs.png"
@@ -58,26 +57,18 @@ It is used to develop computer programs, as well as websites, web apps, web serv
 
 const approved_Woodward_Software = [
   {
-    title: "SQL Server",
+    title: "Woodward SQL Server",
     description: "Microsoft SQL Server is a relational database management system developed by Microsoft. It is a software \
   product with the primary function of storing and retrieving data as requested by other software applications",
     icon: "Assets/sql.png"
   },
   {
-    title: "Goto Meeting",
+    title: "Woodward Goto Meeting",
     description: "Microsoft SQL Server is a relational database management system developed by Microsoft. It is a software \
   product with the primary function of storing and retrieving data as requested by other software applications",
     icon: "Assets/GoTo.png"
   },
 ];
-//sorts everything perfectly
-  var sortByProperty = function (property) {
-    return function (x, y) {
-        //basically returns nothing if they are equal,
-        //or returns the item that is greater than the lesser
-        return ((x[property] === y[property]) ? 0 : ((x[property] > y[property]) ? 1 : -1));
-    };
-  };
 
 
 // Function to read more and restrict text length on each card
@@ -113,19 +104,50 @@ const approved_Woodward_Software = [
     `;
   }
 
+
+
 function categoryTemplate(card) {
-  return `
-    <card class="nested">
-      <img class="image" src="${card.icon}">
-        <div class="text">
-          <!--<button class="request-button" onclick="getDescription('${card.description}')">Request</button> -->
-          <h3>${card.title}</h3>
-          <p>${card.description}</p>
-          <button class="request-button" onclick="show_Office_items()">Request</button>
-      </div>
-    </card>
-  `;
+  if (card.title == "Woodward"){
+    return `
+      <card class="nested">
+        <img class="image" src="${card.icon}">
+          <div class="text">
+            <!--<button class="request-button" onclick="getDescription('${card.description}')">Request</button> -->
+            <h3>${card.title}</h3>
+            <p>${card.description}</p>
+            <button class="request-button" onclick="show_Woodward_items()">Request</button>
+        </div>
+      </card>
+    `;
+  }
+  else if (card.title == "Office"){
+    return `
+      <card class="nested">
+        <img class="image" src="${card.icon}">
+          <div class="text">
+            <!--<button class="request-button" onclick="getDescription('${card.description}')">Request</button> -->
+            <h3>${card.title}</h3>
+            <p>${card.description}</p>
+            <button class="request-button" onclick="show_Office_items()">Request</button>
+        </div>
+      </card>
+    `;
+  }
+  else{
+    return `
+      <card class="nested">
+        <img class="image" src="${card.icon}">
+          <div class="text">
+            <!--<button class="request-button" onclick="getDescription('${card.description}')">Request</button> -->
+            <h3>${card.title}</h3>
+            <p>${card.description}</p>
+            <button class="request-button" onclick="show_Engineering_items()">Request</button>
+        </div>
+      </card>
+    `;
+  }
 }
+
 
 document.getElementById("category-view").innerHTML = `
 <div class="stack">
@@ -135,10 +157,20 @@ document.getElementById("category-view").innerHTML = `
 </div>
 `;
 
+//sorts everything perfectly
+var sortByProperty = function (property) {
+  return function (x, y) {
+      //basically returns nothing if they are equal,
+      //or returns the item that is greater than the lesser
+      return ((x[property] === y[property]) ? 0 : ((x[property] > y[property]) ? 1 : -1));
+  };
+};
+
+//Sorts each list in alphabetical order
 let sorted_Office_List = approved_Office_Software.sort(sortByProperty('title'));
 let sorted_Enginering_List = approved_Engineering_Software.sort(sortByProperty('title'));
 let sorted_Woodward_List = approved_Woodward_Software.sort(sortByProperty('title'));
-// let sorted_Office_List = approved_Office_Software.sort(sortByProperty('title'));
+
 
 // This allows the http div to use the app class.
 // Sets up the structure of the page and sends our Template Literal to two functions for work.
@@ -152,11 +184,21 @@ function show_Office_items() {
   `;
 }
 
-function show_Eng_items() {
+function show_Engineering_items() {
   document.getElementById("app").innerHTML = `
   <div class="stack">
     <main class="grid">
       ${sorted_Enginering_List.map(softTemplate).join("")}
+    </main>
+  </div>
+  `;
+}
+
+function show_Woodward_items() {
+  document.getElementById("app").innerHTML = `
+  <div class="stack">
+    <main class="grid">
+      ${sorted_Woodward_List.map(softTemplate).join("")}
     </main>
   </div>
   `;
